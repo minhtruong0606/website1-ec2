8f2a66778b0e0986c27e5e9045095995b66e79cc
Minh Truong <tmtruong25@gmail.com>
2025-10-07 15:42:30 +0700

    Update connect.php

--- E-Commerce-1-Website-PHP-MYSQL-main/includes/connect.php
+++ E-Commerce-1-Website-PHP-MYSQL-main/includes/connect.php
@@ -1,11 +1,18 @@
-<?php 
-// $con=mysqli_connect('localhost','root','','ecommerce_1');
-$con = new mysqli('localhost','root','','ecommerce_1');
-if(!$con){
-    die(mysqli_error($con));
+<?php
+// Database connection for web2 - Sử dụng database có sẵn của bạn
+$database_name = "web2_database"; // Tên database bạn đã tạo
+$username = "tmdt_user";    // Username bạn đã tạo
+$password = "Nt25102004@";    // Password bạn đã set
+$host = "54.255.146.95";            // Hoặc IP database server
+
+$con = mysqli_connect($host, $username, $password, $database_name);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
 }
 
-
-
-
+// Set charset
+mysqli_set_charset($con, "utf8");
 ?>
