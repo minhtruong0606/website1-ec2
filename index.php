e77e0c31277c1e4db902b9cc40795b83617cfe26
Minh Truong <tmtruong25@gmail.com>
2025-10-24 17:09:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<title>Trang chủ</title>
+		<title>Web của NETNET</title>
 
 		<link rel="shortcut icon" href="img/favicon.png">
 		<!-- Google font -->
